cbd51f0398c18bab99b698a8b3a92dbe94cc9841
Natali <ver4.nata@gmail.com>
2018-11-30 16:32:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log(typeof(null));
+console.log(null == false);
+console.log(null == 0);
+console.log(null == undefined);
+console.log('-----');
+console.log(undefined == false);
+console.log(undefined == 0);
+console.log('-----');
+
+console.log(false == 0);
+
